615eaae6b5dadfe90c55f91c32b1e0a224482954
JoaoLages <joaop.glages@gmail.com>
2022-08-28 12:12:30 +0100

    add LDMTextToImagePipelineExplainer

--- src/diffusers_interpret/attribution.py
+++ src/diffusers_interpret/attribution.py
@@ -12,12 +12,11 @@ def gradient_x_inputs_attribution(pred_logits: torch.Tensor, input_embeds: torch
     # back-prop gradients for all predictions with respect to the inputs and sum them
     print("calculating gradients")
     start = time.time()
-    grad = torch.autograd.grad(tuple_of_pred_logits, input_embeds)[0]
+    grad = torch.autograd.grad(tuple_of_pred_logits, input_embeds, retain_graph=True)[0]
     print("Done", time.time()-start)
 
     # Grad X Input
-    grad_enc_x_input = grad * input_embeds
-    grad_x_input = grad_enc_x_input
+    grad_x_input = grad * input_embeds
 
     # Turn into a scalar value for each input token by taking L2 norm
     feature_importance = torch.norm(grad_x_input, dim=1)

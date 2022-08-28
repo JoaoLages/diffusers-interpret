8481d242e21c043f7515017a38c4821277c136f3
JoaoLages <joaop.glages@gmail.com>
2022-08-28 13:06:17 +0100

    add LDMTextToImagePipelineExplainer

--- src/diffusers_interpret/attribution.py
+++ src/diffusers_interpret/attribution.py
@@ -25,7 +25,7 @@ def gradient_x_inputs_attribution(
     grad_x_input = grad * input_embeds
 
     # Turn into a scalar value for each input token by taking L2 norm
-    feature_importance = torch.norm(grad_x_input, dim=-11)
+    feature_importance = torch.norm(grad_x_input, dim=-1)
 
     if normalize_attributions:
         # Normalize so we can show scores as percentages

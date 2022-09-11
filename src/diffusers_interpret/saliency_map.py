35aba9d921d337e9a4ab82feb400f54a012a2376
JoaoLages <joaop.glages@gmail.com>
2022-09-11 20:47:47 +0100

    change typing

--- src/diffusers_interpret/saliency_map.py
+++ src/diffusers_interpret/saliency_map.py
@@ -36,14 +36,13 @@ class SaliencyMap:
             saliency_map = cv2.cvtColor(saliency_map, cv2.COLOR_BGR2RGB)
             saliency_map = np.float32(saliency_map) / 255.0
 
-            overlayed = (1 - image_weight) * saliency_map + image_weight * img
-            overlayed = overlayed / np.max(overlayed)
-            overlayed = np.uint8(255 * overlayed)
-
             if mask is not None and apply_mask:
                 img *= (1 - mask / 255)
                 saliency_map *= (1 - mask / 255)
-                overlayed *= (1 - mask / 255)
+
+            overlayed = (1 - image_weight) * saliency_map + image_weight * img
+            overlayed = overlayed / np.max(overlayed)
+            overlayed = np.uint8(255 * overlayed)
 
             # Visualize the image and the saliency map
             fig, ax = plt.subplots(1, 3, **kwargs)

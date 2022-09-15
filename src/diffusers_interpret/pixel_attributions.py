533925348edefa508a7e8d2680368709c5776378
JoaoLages <joaop.glages@gmail.com>
2022-09-15 13:15:42 +0100

    try to fix PixelAttributions

--- src/diffusers_interpret/pixel_attributions.py
+++ src/diffusers_interpret/pixel_attributions.py
@@ -6,23 +6,23 @@ from diffusers_interpret.saliency_map import SaliencyMap
 
 
 class PixelAttributions(np.ndarray):
-    def __init__(self, pixel_attributions: np.ndarray, saliency_map: SaliencyMap) -> None:
-        super().__init__(pixel_attributions)
-        self.pixel_attributions = pixel_attributions
-        self.normalized = 100 * (pixel_attributions / pixel_attributions.sum())
-        self.saliency_map = saliency_map
+    def __new__(cls, pixel_attributions: np.ndarray, saliency_map: SaliencyMap):
+        # Construct new ndarray
+        obj = np.asarray(pixel_attributions).view(cls)
+        obj.pixel_attributions = pixel_attributions
+        obj.normalized = 100 * (pixel_attributions / pixel_attributions.sum())
+        obj.saliency_map = saliency_map
 
         # Calculate normalized
         total = sum([attr for _, attr in pixel_attributions])
-        self.normalized = [
+        obj.normalized = [
             (pixel, round(100 * attr / total, 3))
             for pixel, attr in pixel_attributions
         ]
+        return obj
 
     def __getitem__(self, item: Union[str, int]) -> Any:
         return getattr(self, item) if isinstance(item, str) else self.pixel_attributions[item]
 
     def __setitem__(self, key: Union[str, int], value: Any) -> None:
         setattr(self, key, value)
-
-    def __getattr__(self, attr):
